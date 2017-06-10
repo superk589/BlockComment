3fe56addaed28ed65b96e76c84f5d5b18f5627ee
superk589 <superk589@gmail.com>
2017-06-10 22:28:44 +0800

    minor fix

--- BlockCommentExtension/SourceEditorCommand.swift
+++ BlockCommentExtension/SourceEditorCommand.swift
@@ -14,6 +14,8 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        print("--------------command start--------------")
         let buffer = invocation.buffer
         let selections = buffer.selections
         let lines = buffer.lines
@@ -23,10 +25,17 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             let endRange = selections.lastObject as? XCSourceTextRange else {
                 return
         }
+        print("start at \(startRange)")
+        print("end at \(endRange)")
         
         let startLine = startRange.start.line
-        let endLine = endRange.end.line
         
+        if endRange.end.column == 0 {
+            endRange.end.line -= 1
+            endRange.end.column = (lines.object(at: endRange.end.line) as? String)?.characters.count ?? 0
+        }
+        let endLine = endRange.end.line
+
         guard let startString = buffer.lines.object(at: max(0, startLine)) as? String,
             let endString = buffer.lines.object(at: min(endLine, buffer.lines.count - 1)) as? String,
             let preString = buffer.lines.object(at: max(0, startLine - 1)) as? String,
@@ -35,6 +44,11 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
                 return
         }
         
+        print("startLine is \(startString)")
+        print("endLine is \(endString)")
+        print("preLine is \(preString)")
+        print("postLine is \(postString)")
+
         let commented: Bool
         let surrounded: Bool
         
@@ -56,6 +70,9 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             commented = false
             surrounded = false
         }
+        
+        print("commented \(commented)")
+        print("surrounded \(surrounded)")
     
         if commented {
             
@@ -74,21 +91,31 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
             var numberOfIndentationCharacter = Int.max
             for index in startLine...endLine {
                 if let line = lines.object(at: index) as? String {
-                    numberOfIndentationCharacter = min(line.characters.prefix{ $0 == indentationCharacter }.count, numberOfIndentationCharacter)
+                    numberOfIndentationCharacter = min(line.characters.prefix{ [" ", "\t"].contains($0) }.reduce(0){ $0 + ($1 == "\t" ? buffer.tabWidth : 1) }, numberOfIndentationCharacter)
                 }
             }
-            let prefix = String(Array<Character>.init(repeating: indentationCharacter, count: numberOfIndentationCharacter))
             
+            let prefix: String
+            
+            if buffer.usesTabsForIndentation {
+                prefix = String(Array<Character>.init(repeating: "\t", count: numberOfIndentationCharacter / buffer.tabWidth)) + String(Array<Character>.init(repeating: " ", count: numberOfIndentationCharacter % buffer.tabWidth))
+            } else {
+                prefix = String(Array<Character>.init(repeating: " ", count: numberOfIndentationCharacter))
+            }
+            
+            
+            print("insert \"\(prefix)*/\"at line \(endLine + 1)")
+            lines.insert("\(prefix)*/", at: endLine + 1)
+            print("insert \"\(prefix)/*\"at line \(startLine)")
             lines.insert("\(prefix)/*", at: startLine)
-            lines.insert("\(prefix)*/", at: endLine + 2)
             
-            startRange.start.line = min(endRange.end.line, startRange.start.line + 1)
-//            startRange.end.line = min(startRange.end.line, startRange.start.line + 1)
-            
-//            endRange.start.line += 1
-//            endRange.end.line += 1
+            if startRange.start.column == 0 {
+                startRange.start.line += 1
+            }
             
         }
+        
+        print("--------------command end--------------")
     
         completionHandler(nil)
     }
